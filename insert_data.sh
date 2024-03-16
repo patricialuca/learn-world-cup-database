b64598f47d3588d10e419d4c2733efd9971d2b1f
Patricia Luengo Carretero <patricialuengocarretero@gmail.com>
2024-03-16 12:21:35 +0000

    creacion base de datos

--- insert_data.sh
+++ insert_data.sh
@@ -3,8 +3,13 @@
 if [[ $1 == "test" ]]
 then
   PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+  echo "Test"
 else
   PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+  echo "Prod"
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+done
